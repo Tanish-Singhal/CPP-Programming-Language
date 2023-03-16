7e83375ccab2deb350444fe98b760807b347f5de
Tanish Singhal <114289953+Tanish-Singhal@users.noreply.github.com>
2023-03-16 22:31:51 +0530

    Update 04_pattern.cpp

--- 04_patterns.cpp
+++ 04_patterns.cpp
@@ -27,6 +27,24 @@ void triangular_box(int num) {
     }
 }
 
+void triangular_no(int num) {
+    for (int i = 1; i < num; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout<< j << " ";
+        }
+        cout<<endl;
+    }
+}
+
+void triangular_sameno(int num) {
+    for (int i = 1; i < num; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout<< i << " ";
+        }
+        cout<<endl;
+    }
+}
+
 int main() {
     
     int n1;
