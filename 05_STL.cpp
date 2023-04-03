4adbaa40a08dd92eac3db7ba9861af07c7793ad3
Tanish Singhal <tanishsinghal510@gmail.com>
2023-04-03 17:31:02 +0530

    updated the file

--- 05_STL.cpp
+++ 05_STL.cpp
@@ -4,28 +4,6 @@
 using namespace std;
 
 // TODO:
-void explainPair() {
-
-// Pair is a way of creating a Composite-Datatype composed of 2 different primitive/composite datatypes.
-// It can be useful whe you wan to return multiple values from a single function
-
-    pair<int, int> p = {1, 3};
-    pair<int, string> q = {4, "hello"};      // char, float, etc can also be used
-    
-    cout<< p.first << " " << p.second <<endl;
-    cout<< q.first << " " << q.second <<endl; 
-
-    pair<int, pair<int, int>> r = {1, {3, 4}};
-    cout<< p.first << " " << r.second.second << " " << r.second.first <<endl;
-
-    q.first = 17; 
-    cout<< q.first << " " << q.second <<endl;
-
-    pair<int, int> arr[] = {{1, 2}, {2, 5}, {5, 1}};
-    cout<< arr[1].second <<endl;
-}
-
-// TODO:
 void explainVector() {      // v.clear() => O(n)    // rest other things => O(1)
 
 // It is a dynamic sized array. Number of elements can be increased or decreased. 
@@ -38,11 +16,12 @@ void explainVector() {      // v.clear() => O(n)    // rest other things => O(1)
     // v.push_back(x) - insert the value x to the end of the vector. O(1)
     // v.emplace_back(x) - similar to puch_back but faster then push_back
     // v.pop_back()   - erase the last element. O(1)
-    // v.clear() - erase all elements. O👎
+    // v.clear() - erase all elements. O(n)
     // v.size() - returns the current size of the vector. O(1)
     // [] operator - can be used to access elements like an array. O(1)
     
-    cout << v[0]; // prints the first element in the vector
+    cout << v[0];           // prints the first element in the vector
+    cout << v[3];
 
     vector<int> v;
     v.push_back(5);
@@ -63,18 +42,19 @@ void explainVector() {      // v.clear() => O(n)    // rest other things => O(1)
     cout<< v.size() <<endl;                                   // 0
 
 
-    // synatax of push_back and embrace_back
+    // syntax of push_back and emplace_back
     vector<pair<int, int>> vec;
     vec.push_back({1, 2});
     vec.emplace_back(1, 2);
 
-    vector<int> v1(5, 20);
+    vector<int> v1(5, 20);             // {20, 20, 20, 20, 20}
     // vector<int> v2(v1);             // Now this we make a copy of container v1
 
     // Accessing data from vector
     cout<< v1[0] << " " << v1[4];      // 0 and 4 are the index of vector
 
-    // Access the memory location and value         v = {10, 20, 30, 40, 50}
+    // Access the memory location and value         
+    vector<int> v = {10, 20, 30, 40, 50};
     vector<int>::iterator it = v.begin();          // 10 -> 135648
 
     it++;                                          
@@ -83,7 +63,7 @@ void explainVector() {      // v.clear() => O(n)    // rest other things => O(1)
     it = it + 2;
     cout<< *(it) << " ";                           // 40
 
-    vector<int>::iterator it = v.end();           // it will point he location just after the last element in vector
+    vector<int>::iterator it = v.end();            // it will point he location just after the last element in vector
     it--;
     cout<< *(it) << " ";                           // 50
 
@@ -99,21 +79,52 @@ void explainVector() {      // v.clear() => O(n)    // rest other things => O(1)
     }
 
     // Deleteing multiple element in vector
-    // v = {10, 20, 30, 40, 50}
+    vector<int> v = {10, 20, 30, 40, 50};
+    v.erase(v.begin()+1);                  // v = {10, 30, 40, 50}
+    
+    vector<int> v = {10, 20, 30, 40, 50};
     v.erase(v.begin()+2, v.begin()+4);     // v = {10, 40, 50}
     //     starting        ending + 1
 
     // Insert Elements in Vector
     vector<int> v(2, 100);          // {100, 100}
-    v.insert(v.begin(), 300);       // {300, 100, 100}
-    v.insert(v.begin()+1, 2, 10);    // {300, 10, 10, 100, 100}
+    v.insert(v.begin()+1, 3);       // {100, 3, 100}
+    v.insert(v.begin()+1, 2, 5);    // {100, 5, 5, 3, 100}
 
     vector<int> copy{2, 50};        // {50, 50}
     v.insert(v.begin(), copy.begin(), copy.end());  // {50, 50, 300, 10, 10, 100, 100}
 
+    cout<< v.size();
+    v.clear();
+    cout<< v.empty();       // 1-> True & 0-> False
+
     vector<int> v8 = {10, 20};
     vector<int> v9 = {30, 40};
     v8.swap(v9);    // v8 ={30,40}   v9 ={10,20}
+
+}
+
+// TODO:
+void explainPair() {
+
+// Pair is a way of creating a Composite-Datatype composed of 2 different primitive/composite datatypes.
+// It can be useful whe you wan to return multiple values from a single function
+
+    pair<int, int> p = {1, 3};
+    pair<int, string> q = {4, "hello"};      // char, float, etc can also be used
+    
+    cout<< p.first << " " << p.second <<endl;   //  1 3
+    cout<< q.first << " " << q.second <<endl;   // 4 hello
+
+    pair<int, pair<int, int>> r = {1, {3, 4}};
+    cout<< r.first << " " << r.second.second << " " << r.second.first <<endl;   // 1 3 4
+
+    q.first = 17;                               // updating the pairs
+    cout<< q.first << " " << q.second <<endl;   // 17 hello
+
+    pair<int, int> arr[] = {{1, 2}, {2, 5}, {5, 1}};
+    cout<< arr[1].second <<endl;
+
 }
 
 // TODO:
@@ -126,12 +137,13 @@ void explainlist() {
     ls.emplace_back(4); // {2, 4}
 
     ls.push_front(5);   // {5, 2, 4}
-    // Why we use list because insert operation in vector is costlier than front
+    // Why we use list because insert operation in vector is costlier than front (Time complexity is high)
 
     ls.emplace_front(); // {2, 4}
 
     // rest function are same as vector
     // begin, end, rbegin, rend, clear, insert, size, swap
+
 }
 
 // TODO:
@@ -154,7 +166,9 @@ void explainDeque() {
 
     // rest function are same as vector
     // begin, end, rbegin, rend, clear, insert, size, swap
+
 }
+
 
 // TODO:
 void expalinStack() {       // all operations are of O(1)
@@ -172,7 +186,7 @@ void expalinStack() {       // all operations are of O(1)
 
     cout<< st.top();    // print 5     " str[2] is invalid here"
 
-    st.pop();           // st look like (3, 3, 2, 1)
+    st.pop();       // st look like (3, 3, 2, 1)
 
     cout<< st.top();    // 3
 
@@ -184,6 +198,7 @@ void expalinStack() {       // all operations are of O(1)
     st1.swap(st2);
 
 }
+
 
 // TODO:
 void explainQueue() {       // all operations are of O(1)
@@ -197,11 +212,11 @@ void explainQueue() {       // all operations are of O(1)
     q.push(2);      // {1, 2}
     q.emplace(4);   // {1, 2, 4}
 
-    q.back() += 5;
+    q.back() += 5;  // 9 (4+5=9)
 
     cout<< q.back();    // 9
 
-    // 0 is {1, 2, 9}
+    // q is {1, 2, 9}
     cout<< q.front();   // print 1
 
     q.pop();    // {2, 9}
@@ -211,6 +226,7 @@ void explainQueue() {       // all operations are of O(1)
     // size swap empty same as stack
 
 }
+
 
 // TODO:
 void explainPriorityQueue() {                                // MAX HEAP
